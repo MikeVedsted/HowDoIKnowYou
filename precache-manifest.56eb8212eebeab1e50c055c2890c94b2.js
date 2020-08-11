b49e06ef4a51e40d6bb57f50604218ea3b567a0b
Mike Vedsted <mikevedsted@gmail.com>
2020-08-11 16:02:23 +0300

    Updates

--- precache-manifest.56eb8212eebeab1e50c055c2890c94b2.js
+++ precache-manifest.56eb8212eebeab1e50c055c2890c94b2.js
@@ -1,6 +1,6 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "dcbc45f045702ac944253ec9b5a26610",
+    "revision": "5143b1dc70158abf284e0527ca53abd3",
     "url": "/HowDoIKnowYou/index.html"
   },
   {
@@ -8,7 +8,7 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/HowDoIKnowYou/static/css/2.3c03f26f.chunk.css"
   },
   {
-    "revision": "1b7498aedc6f7105721b",
+    "revision": "b6e9800f04453623fc20",
     "url": "/HowDoIKnowYou/static/css/main.93aa5b0f.chunk.css"
   },
   {
@@ -20,8 +20,8 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/HowDoIKnowYou/static/js/2.33fe4688.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "1b7498aedc6f7105721b",
-    "url": "/HowDoIKnowYou/static/js/main.f56aafb1.chunk.js"
+    "revision": "b6e9800f04453623fc20",
+    "url": "/HowDoIKnowYou/static/js/main.ed97af03.chunk.js"
   },
   {
     "revision": "b196529536b4dbb02d51",
