349e9b07a774a99e4beaa591ab36bc22429e8987
Mike Vedsted <mikevedsted@gmail.com>
2020-08-11 16:56:23 +0300

    on purpose

--- src/api/api.js
+++ src/api/api.js
@@ -2,7 +2,7 @@ export const apiCall = (link) =>
 	fetch(link, {
 		method: 'GET',
  		headers: {
-  			'X-API-KEY': REACT_APP_FACES_KEY,
+  			'X-API-KEY': '63DBCC75-37064B86-8666620F-09A37CFD',
  			'Accept': 'application/json',
   			'Cache-Control': 'no-cache'
  		}
